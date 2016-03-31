bae8c5bcd6c2a924fb6121631b6449b724e46c9d
Andrey <zoxy1@yandex.ru>
2016-03-31 15:41:17 +0700

    программа считает выражения и выводит на экран

--- labs/labs/main.cpp
+++ labs/labs/main.cpp
@@ -9,35 +9,33 @@ int main()
     SetConsoleOutputCP(1251); // установка кодовой страницы win-cp 1251 в поток вывода
 	/* Для правильного отображения русских символов в консоли нужно выбрать 
 	в свойствах консоли шрифт Lucida Console */
+int a = 2 + (1 / 4); 
+float a_float = 7 * 2 + 1 ^ 3; 
+int a2 = 4 * 3 ^ 3 + 1; 
 
-	char Second_name[20];
-	char First_name[20];
-	char Patronymic_name[20]; 
-	int _month, Temp,_date;
-	double _year_double;
-	float _year_float;
-	cout<<"Введите фамилию имя отчество (через пробел): ";
-	cin >> Second_name;
-	cin >> First_name;
-	cin >> Patronymic_name;
-	system("cls");
-	cout<<"Введите дату рождения: ";
-	cin >> _date;
-	system("cls");
-	cout<<"Введите месяц рождения(от 1 до 12): ";
-	cin >> _month;
-	system("cls");
-	cout<<"Введите год рождения: ";
-	cin >> Temp;
-	_year_float=Temp+40;
-	_year_double=Temp+40;
-	system("cls");
-	cout << "Студент:" << Second_name<<" "<<First_name<<" "<<Patronymic_name;
-	cout <<" родился "<<_date<<"."<<_month<<"."<<Temp<<"г"<<endl;
-	cout << "\nему исполнится 40 лет в:" <<endl;
-	cout << fixed <<_year_double <<" году(выводится тип double)"<< endl;
-	cout << fixed <<_year_float  <<" году(выводится тип float)"<<endl;
-	system("pause"); // Команда задержки экрана
+int j, b = 5, a3 =4 ; j = b---a3;
+
+int j1, b1 = 5, a4 =4 ; j1 = (b1--)-a4;
+
+int j2, b2 = 5, a5 =4 ; j2 = b2-(--a5);
+
+int j3, b3=5, a6=4; j3 = --b3-a6--;
+
+int j4, b4=5, a7=4; j4 = --(b4=a7);
+
+cout<<"7 - 2 * ( 5 / 2 )="<<(7 - 2 * ( 5 / 2 ))<<endl;
+cout<<"7 - (2 * 5) / 2 ="<<(7 - (2 * 5) / 2 )<<endl;
+cout<<"7 - 2 * ( (float)5 / 2 ) ="<<(7 - 2 * ( (float)5 / 2 ) )<<endl;	
+cout<<"int a = 2 + (1 / 4); a ="<<a<<endl;
+cout<<"int a2 = 4 * 3 ^ 3 + 1; a2 ="<<a2<<endl;
+cout<<"int j, b = 5, a3 =4 ; j = b---a3; j="<<j<<endl;
+cout<<"int j1, b1 = 5, a4 =4 ; j1 = (b1--)-a4; j1="<<j1<<endl;
+cout<<"int j2, b2 = 5, a5 =4 ; j2 = b2-(--a5); j2="<<j2<<endl;
+cout<<"int j3, b3=5, a6=4; j3 = --b3-a6--; j3="<<j3<<endl;
+cout<<"int j4, b4=5, a7=4; j4 = --(b4=a7); j4="<<j4<<endl;
+
+
+system("pause"); // Команда задержки экрана
 	return 0;
 }
 
