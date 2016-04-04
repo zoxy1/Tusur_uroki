655bc8dd7158b4ff5de2ee8aeb90eface1205899
zoxy1 <zoxy1@yandex.ru>
2016-04-04 21:36:13 +0700

    2

--- labs/labs/main.cpp
+++ labs/labs/main.cpp
@@ -14,10 +14,15 @@ int main()
     SetConsoleOutputCP(1251); // установка кодовой страницы win-cp 1251 в поток вывода
 	/* Для правильного отображения русских символов в консоли нужно выбрать 
 	в свойствах консоли шрифт Lucida Console */
+	struct_student *p_student;
+	p_student = new struct_student;
+	p_student->rost=154;
+	p_student->years=20;
 	struct_student student1;
 	student1.rost=182;
 	student1.years=30;
-	
+	cout << p_student->rost <<endl;
+	cout << p_student->years <<endl;
 	cout << student1.rost <<endl;
 	cout << student1.years <<endl;
 	system("pause"); // Команда задержки экрана
