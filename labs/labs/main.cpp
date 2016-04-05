fddc738db94d9e2727ed23d2cfa4e718a15e270e
zoxy1 <zoxy1@yandex.ru>
2016-04-05 21:05:30 +0700

    сделал программу нахождения минимального максимального числа в массиве

--- labs/labs/main.cpp
+++ labs/labs/main.cpp
@@ -1,4 +1,4 @@
-// lab2_1
+// lab3_2
 #include <iostream> // содержит функции ввода вывода cin, cout
 #include<windows.h> //содержит функции SetConsoleCP(), SetConsoleOutputCP()
 #include <ctime> // библиотека необходимая для использования функции time()
@@ -10,43 +10,41 @@ int main()
     SetConsoleOutputCP(1251); // установка кодовой страницы win-cp 1251 в поток вывода
 	/* Для правильного отображения русских символов в консоли нужно выбрать 
 	в свойствах консоли шрифт Lucida Console */
-	srand( time( 0 ) );
+	srand( (unsigned int)time( 0 ) ); //используется для того чтобы при новом запуске программы генерировались разные числа в функции rand()
 	
-	int elem_mass=0,interval_mass=0,summa=0,summa_max=0, index_interval_max=0;
-	cout<<"Введите кол545ичество элементов массива от 1 до 100:";
+	int elem_mass=0, max_value=0;
+	int min_value=32767; //максимальное число которое генерит функция rand() 
+	cout<<"Введите количество элементов массива:";
 	cin >> elem_mass; 
-	cout<<"Введите интервал от 0 до "<<elem_mass<<":";
-	cin >> interval_mass;
+	
 	int *mass = new int[elem_mass]; // динамическое выделение памяти под массив типа int размером в elem_mass 
 	
 	for(int i=0;i<elem_mass;i++) //заполняем массив случайными числами от 0 до 10
 	{
-		mass[i]=rand()%10;
+		mass[i]=rand();
 	}
 	for(int i=0;i<elem_mass;i++) // Выводим заполненный массив чисел
 	{
 		cout <<i<<"- элемент:" <<mass[i]<<endl;
 	}
-	
-	for(int j=0;j<=(elem_mass-interval_mass);j++) // находим максимальную сумму в заданном интервале
+	for(int i=0;i<elem_mass;i++) // вычисляем минимальное и максимальное значение в массиве
 	{
-		summa=0;
-		for(int i=0;i<interval_mass;i++)
+		if(max_value<mass[i]) 
 		{
-			summa=summa+mass[i+j];
+			max_value=mass[i];
 		}
-		if(summa_max<summa) 
+		if(min_value>mass[i])
 		{
-			summa_max=summa;
-			index_interval_max=j; // с этого индекса начинается интервал с максимальной суммой
+			min_value=mass[i];
 		}
-	}
 	
-	cout <<"Интервал с максимальной суммой равной: " <<summa_max<<endl; // Выводим максимальную сумму
-	for(int i=index_interval_max;i<(index_interval_max+interval_mass);i++) // выводим интервал с максимальной суммой
-	{
-		cout <<i<<"- элемент:" <<mass[i]<<endl;
 	}
+
+	cout <<"Максимальное значение в массиве равно: " <<max_value<<endl;
+	cout <<"Минимальное значение в массиве равно: " <<min_value<<endl;
+	
+	
+	
 	//free(mass); //осво бождаем память выделенную под динамический массив 
 	delete []mass;
 	system("pause"); // Команда задержки экрана
