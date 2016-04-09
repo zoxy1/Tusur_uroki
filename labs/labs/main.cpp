0650461af1bf92af99bc2152524fed01886decbe
Andrey <zoxy1@yandex.ru>
2016-04-09 22:42:01 +0600

    сделал массив

--- labs/labs/main.cpp
+++ labs/labs/main.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 void Vivod(int elementCount, int *Array) //функция вывода на консоль массива с заданным количеством элементов
 {
-for(int i=0;i<elementCount;i++) // Выводим заполненный массив 2 чисел
+for(int k=0;k<elementCount;k++) // Выводим заполненный массив 
 	{
-		cout <<i<<" : "<<Array[i]<<endl;
+		cout <<k<<" : "<<Array[k]<<endl;
 	}	
 }
 
@@ -19,70 +19,80 @@ int main()
     SetConsoleOutputCP(1251); // установка кодовой страницы win-cp 1251 в поток вывода
 	/* Для правильного отображения русских символов в консоли нужно выбрать 
 	в свойствах консоли шрифт Lucida Console */
+	int a[10];
+	int *p; 
+	p=a; // указателю p присваиваем адрес первого элемента массива a[] (p=a[0])
+	for(int j=0;j<10;j++){
+		*(p+j)=j; //заполняем массив с помощью указателя числами 0 1 2 3 4 5 6 7 8 9 
+	}
+	Vivod(10,p);
 	
-	srand( time( 0 ) ); //используется для того чтобы при новом запуске программы генерировались разные числа в функции rand()
-	int elem_mass1=0,elem_mass2=0;
-	int chislo;
-	cout<<"Введите количество элементов 1-го массива от 1 до 10: ";
-	cin >> elem_mass1; 
-	int *mass1 = new int[elem_mass1]; // динамическое выделение памяти под массив типа int размером в elem_mass1 
-	int *mass1_plus = new int[elem_mass1]; // динамическое выделение памяти под массив типа int размером в elem_mass1 
-	int *mass1_minus = new int[elem_mass1]; // динамическое выделение памяти под массив типа int размером в elem_mass1 
-	int *mass1_umnogit = new int[elem_mass1]; // динамическое выделение памяти под массив типа int размером в elem_mass1 
-	cout<<"Введите количество элементов 2-го массива от 1 до 10: ";
-	cin >> elem_mass2; 
-	int *mass2 = new int[elem_mass2]; // динамическое выделение памяти под массив типа int размером в elem_mass2 
-	int *mass2_plus = new int[elem_mass2]; // динамическое выделение памяти под массив типа int размером в elem_mass2 
-	int *mass2_minus = new int[elem_mass2]; // динамическое выделение памяти под массив типа int размером в elem_mass2
-	int *mass2_umnogit = new int[elem_mass2]; // динамическое выделение памяти под массив типа int размером в elem_mass2
-	for(int i=0;i<elem_mass1;i++) //заполняем массивы случайными числами от 0 до 10
-	{
-		mass1[i]=rand()%10;
-		
-	}
-	for(int i=0;i<elem_mass2;i++) //заполняем массивы случайными числами от 0 до 10
-	{
-		mass2[i]=rand()%10;
-	}
-	cout<<"\nВыводим первый массив:\n";
-	Vivod(elem_mass1, mass1);
-	cout<<"\nВыводим второй массив:\n";
-	Vivod(elem_mass2, mass2);
-	cout<<"\nВведите число для сложения, вычитания, умножения:";
-	cin>>chislo;
-	for(int i=0;i<elem_mass1;i++) // прибавляем, вычитаем, умножаем число chislo
-	{
-		mass1_plus[i]=mass1[i]+chislo;
-		mass1_minus[i]=mass1[i]-chislo;
-		mass1_umnogit[i]=mass1[i]*chislo;
-	}
-	cout<<"\nПервый массив при сложении с числом:\n";
-	Vivod(elem_mass1, mass1_plus);
-	cout<<"\nПервый массив при вычитании числа:\n";
-	Vivod(elem_mass1, mass1_minus);
-	cout<<"\nПервый массив при умножении на число:\n";
-	Vivod(elem_mass1, mass1_umnogit);
 	
-	for(int i=0;i<elem_mass2;i++) // прибавляем, вычитаем, умножаем на число chislo
-	{
-		mass2_plus[i]=mass2[i]+chislo;
-		mass2_minus[i]=mass2[i]-chislo;
-		mass2_umnogit[i]=mass2[i]*chislo;
-	}
-	cout<<"\nВторой массив при сложении с числом:\n";
-	Vivod(elem_mass2, mass2_plus);
-	cout<<"\nВторой массив при вычитании числа:\n";
-	Vivod(elem_mass2, mass2_minus);
-	cout<<"\nВторой массив при умножении на число:\n";
-	Vivod(elem_mass2, mass2_umnogit);
-	delete []mass1; // высвобождаем памать выделенную под массивы
-	delete []mass2; 
-	delete []mass1_plus; 
-	delete []mass1_minus;
-	delete []mass1_umnogit; 
-	delete []mass2_plus; 
-	delete []mass2_minus;
-	delete []mass2_umnogit; 
+	cout <<"Значение указуемой переменной:"<<*p<<endl; // выводим значение переменной а которая находистся по адресу p
+
+	//srand( time( 0 ) ); //используется для того чтобы при новом запуске программы генерировались разные числа в функции rand()
+	//int elem_mass1=0,elem_mass2=0;
+	//int chislo;
+	//cout<<"Введите количество элементов 1-го массива от 1 до 10: ";
+	//cin >> elem_mass1; 
+	//int *mass1 = new int[elem_mass1]; // динамическое выделение памяти под массив типа int размером в elem_mass1 
+	//int *mass1_plus = new int[elem_mass1]; // динамическое выделение памяти под массив типа int размером в elem_mass1 
+	//int *mass1_minus = new int[elem_mass1]; // динамическое выделение памяти под массив типа int размером в elem_mass1 
+	//int *mass1_umnogit = new int[elem_mass1]; // динамическое выделение памяти под массив типа int размером в elem_mass1 
+	//cout<<"Введите количество элементов 2-го массива от 1 до 10: ";
+	//cin >> elem_mass2; 
+	//int *mass2 = new int[elem_mass2]; // динамическое выделение памяти под массив типа int размером в elem_mass2 
+	//int *mass2_plus = new int[elem_mass2]; // динамическое выделение памяти под массив типа int размером в elem_mass2 
+	//int *mass2_minus = new int[elem_mass2]; // динамическое выделение памяти под массив типа int размером в elem_mass2
+	//int *mass2_umnogit = new int[elem_mass2]; // динамическое выделение памяти под массив типа int размером в elem_mass2
+	//for(int i=0;i<elem_mass1;i++) //заполняем массивы случайными числами от 0 до 10
+	//{
+	//	mass1[i]=rand()%10;
+	//	
+	//}
+	//for(int i=0;i<elem_mass2;i++) //заполняем массивы случайными числами от 0 до 10
+	//{
+	//	mass2[i]=rand()%10;
+	//}
+	//cout<<"\nВыводим первый массив:\n";
+	//Vivod(elem_mass1, mass1);
+	//cout<<"\nВыводим второй массив:\n";
+	//Vivod(elem_mass2, mass2);
+	//cout<<"\nВведите число для сложения, вычитания, умножения:";
+	//cin>>chislo;
+	//for(int i=0;i<elem_mass1;i++) // прибавляем, вычитаем, умножаем число chislo
+	//{
+	//	mass1_plus[i]=mass1[i]+chislo;
+	//	mass1_minus[i]=mass1[i]-chislo;
+	//	mass1_umnogit[i]=mass1[i]*chislo;
+	//}
+	//cout<<"\nПервый массив при сложении с числом:\n";
+	//Vivod(elem_mass1, mass1_plus);
+	//cout<<"\nПервый массив при вычитании числа:\n";
+	//Vivod(elem_mass1, mass1_minus);
+	//cout<<"\nПервый массив при умножении на число:\n";
+	//Vivod(elem_mass1, mass1_umnogit);
+	//
+	//for(int i=0;i<elem_mass2;i++) // прибавляем, вычитаем, умножаем на число chislo
+	//{
+	//	mass2_plus[i]=mass2[i]+chislo;
+	//	mass2_minus[i]=mass2[i]-chislo;
+	//	mass2_umnogit[i]=mass2[i]*chislo;
+	//}
+	//cout<<"\nВторой массив при сложении с числом:\n";
+	//Vivod(elem_mass2, mass2_plus);
+	//cout<<"\nВторой массив при вычитании числа:\n";
+	//Vivod(elem_mass2, mass2_minus);
+	//cout<<"\nВторой массив при умножении на число:\n";
+	//Vivod(elem_mass2, mass2_umnogit);
+	//delete []mass1; // высвобождаем памать выделенную под массивы
+	//delete []mass2; 
+	//delete []mass1_plus; 
+	//delete []mass1_minus;
+	//delete []mass1_umnogit; 
+	//delete []mass2_plus; 
+	//delete []mass2_minus;
+	//delete []mass2_umnogit; 
 	system("pause"); //  Команда задержки экрана
 	return 0;
 }
