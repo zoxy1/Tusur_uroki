9bddc8eebc37d29707eb61cb83fda3dc2cc715e9
zoxy1 <zoxy1@yandex.ru>
2016-04-18 23:02:06 +0700

    написал функцию подсчета суммы элементов двух массивов и вывода какая больше

--- labs/labs/main.cpp
+++ labs/labs/main.cpp
@@ -1,8 +1,26 @@
-// lab4_1
+// lab5_2
 #include <iostream> // содержит функции ввода вывода cin, cout
 #include<windows.h> //содержит функции SetConsoleCP(), SetConsoleOutputCP()
-#include<math.h> // содержит функцию sin(x);
+#include <ctime> // библиотека необходимая для использования функции time()
 using namespace std;
+
+void fuction_srav(int * mass1, int elem_mass1, int * mass2, int elem_mass2)
+{
+long int summa1=0,summa2=0;
+	for(int i=0;i<elem_mass1;i++)
+	{
+	summa1+=mass1[i];
+	}
+	for(int i=0;i<elem_mass2;i++)
+	{
+	summa2+=mass2[i];
+	}
+	if (summa1>summa2) 
+		cout<<"Сумма элементов в первом массиве > чем во втором."<<endl;
+	else 
+		cout<<"Сумма элементов в первом массиве < чем во втором."<<endl;
+
+}
 
 int main()
 {
@@ -10,42 +28,44 @@ int main()
     SetConsoleOutputCP(1251); // установка кодовой страницы win-cp 1251 в поток вывода
 	/* Для правильного отображения русских символов в консоли нужно выбрать 
 	в свойствах консоли шрифт Lucida Console */
-	double d_left=0, d_right=0,shag=0, sinx_f=0,sinx_d=0;
+	srand( time( 0 ) ); //используется для того чтобы при новом запуске программы генерировались разные числа в функции rand()
 	
-	cout<<"Введите через пробел диапазон изменения аргумента синуса:";
-	cin>>d_left; //вводим левую границу диапазона аргумента х 
-	cin>>d_right; //вводим правую границу диапазона аргумента х 
-	cout<<"Введите шаг изменения аргумента х в sin(х):";
-	cin>>shag; //вводим шаг аргумента х 
+	int elem_mass1=0,elem_mass2=0;
 	
-	cout.precision(10); // устанавливаем на вывод точность десять десятичных знаков
-	for(d_left;d_left<=d_right;d_left+=shag)
-		{
-			sinx_d=sin(d_left); //находим значение синуса для аргумента типа double
-			sinx_f=sin((float)d_left); //находим значение синуса для аргумента типа float
-			cout<<endl<<endl<<"При х="<<d_left<<endl; // выводим значение аргумента
-			cout<<"double:"<<sinx_d<<endl;	// выводим значение синуса для аргумента типа double
-			cout<<"float: "<<sinx_f; // выводим значение синуса для аргумента типа float
-			if((sinx_d!=0)||(sinx_f!=0)) 
-			{
-				// сравниваем значение синуса для аргумента типа double и float
-				if(sinx_d>sinx_f) 
-				{
-					cout<<endl<<"Значение sin((double)х)>sin((float) x)"<<endl;
-				}
-				else
-				{
-					cout<<endl<<"Значение sin((double)х)<sin((float) x)"<<endl;
-				}
-			}
-			if(sin(d_left)==sin((float)d_left))
-				{
-					cout<<endl<<"Значение sin((double)х)==sin((float) x)"<<endl;
-				}
-		
+	cout<<"Введите количество элементов первого массива:";
+	cin >> elem_mass1; 
+	cout<<"Введите количество элементов первого массива:";
+	cin >> elem_mass2; 
 	
-		}
-	system("pause"); //  Команда задержки экрана
+	int *mass1 = new int[elem_mass1]; // динамическое выделение памяти под массив типа int размером в elem_mass1 
+	int *mass2 = new int[elem_mass2]; // динамическое выделение памяти под массив типа int размером в elem_mass1 
+
+	for(int i=0;i<elem_mass1;i++) //заполняем массив случайными числами от 0 до 10
+	{
+		mass1[i]=rand();
+	}
+	for(int i=0;i<elem_mass2;i++) //заполняем массив случайными числами от 0 до 10
+	{
+		mass2[i]=rand();
+	}
+	
+	cout<<endl<<"Выводим первый массив"<<endl;
+	for(int i=0;i<elem_mass1;i++) // Выводим заполненный первый массив чисел
+	{
+		cout <<i<<"- элемент:" <<mass1[i]<<endl;
+	}
+	cout<<endl<<"Выводим второй массив"<<endl;
+	for(int i=0;i<elem_mass2;i++) // Выводим заполненный первый массив чисел
+	{
+		cout <<i<<"- элемент:" <<mass2[i]<<endl;
+	}
+	
+	// функция подсчитывает сумму элементов в двух массивах и выводит какая из них больше 
+	fuction_srav(mass1, elem_mass1, mass2, elem_mass2); 
+	
+	delete []mass1;
+	delete []mass2;
+	system("pause"); // Команда задержки экрана
 	return 0;
 }
 
