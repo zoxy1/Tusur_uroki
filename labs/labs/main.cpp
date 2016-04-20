e5d153725c20b516bfa997f02f7324b61aec5db3
zoxy1 <zoxy1@yandex.ru>
2016-04-20 21:06:05 +0700

    +

--- labs/labs/main.cpp
+++ labs/labs/main.cpp
@@ -14,7 +14,7 @@ int main()
 	/* Для правильного отображения русских символов в консоли нужно выбрать 
 	в свойствах консоли шрифт Lucida Console */
 	 
-	int count1=0,str_count=0;
+	int count1=0,str_count=0,chislo=1,posishin=0;
 	setlocale(LC_ALL,".1251");                //Делаем допустимым вывод кириллицы в консоль.
      FILE *in = fopen("Text.txt","r"); //файл находится в папке main (папка main находится в папке с проектом)
       cout<<"Открываем файл \"Text.txt\" ";                                                  //  параметр "r", то есть reed - позволит нам только считать информацию.
@@ -27,16 +27,29 @@ int main()
         char str[12];       //создаём строку(string) .
         fgets(str,12,in);   //записываем в str первую строку.
         str_count++;
+		
 		for(int i=0;i<10;i++)
 		{
 			if(str[i]=='0'||str[i]=='1'||str[i]=='2'||str[i]=='3'||str[i]=='4'||str[i]=='5'||str[i]=='6'||str[i]=='7'||str[i]=='8'||str[i]=='9')
 			{
-			count1++;
+				
+				if((chislo==1)&&(posishin<(i+1)))
+				{
+				count1++;
+				}
+			chislo=1;
+			posishin=i;
 			}
+			else
+			{
+			chislo=0;
+			}
+		
 		}
 		cout<<endl<<"Строка "<<str_count<<" : "<<str<<endl; 
 		cout<<"Количество цифр в строке:"<<count1<<endl;  
-		 while(!feof(in))    //пока не конец файла in ,мы исполняем цикл.
+		
+		while(!feof(in))    //пока не конец файла in ,мы исполняем цикл.
          {
         
 			fgets(str,12,in); 
