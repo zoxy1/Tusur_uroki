8ad37a350782b205b3b8e0866a49bff7c375f7b1
zoxy1 <zoxy1@yandex.ru>
2016-04-22 22:44:16 +0700

    +

--- labs/labs/main.cpp
+++ labs/labs/main.cpp
@@ -1,23 +1,21 @@
-// lab4_1
+// lab5_1
 #include <iostream> // содержит функции ввода вывода cin, cout
 #include<windows.h> //содержит функции SetConsoleCP(), SetConsoleOutputCP()
 #include<math.h> // содержит функцию sin(x);
 using namespace std;
 
-int main()
+void main()
 {
 	SetConsoleCP(1251);// установка кодовой страницы win-cp 1251 в поток ввода
     SetConsoleOutputCP(1251); // установка кодовой страницы win-cp 1251 в поток вывода
 	/* Для правильного отображения русских символов в консоли нужно выбрать 
 	в свойствах консоли шрифт Lucida Console */
 	double d_left=0, d_right=0,shag=0, sinx_f=0,sinx_d=0;
-	
 	cout<<"Введите через пробел диапазон изменения аргумента синуса:";
 	cin>>d_left; //вводим левую границу диапазона аргумента х 
 	cin>>d_right; //вводим правую границу диапазона аргумента х 
 	cout<<"Введите шаг изменения аргумента х в sin(х):";
 	cin>>shag; //вводим шаг аргумента х 
-	
 	cout.precision(10); // устанавливаем на вывод точность десять десятичных знаков
 	for(d_left;d_left<=d_right;d_left+=shag)
 		{
@@ -46,6 +44,5 @@ int main()
 	
 		}
 	system("pause"); //  Команда задержки экрана
-	return 0;
 }
 
