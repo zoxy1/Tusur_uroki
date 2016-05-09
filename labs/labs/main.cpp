4651de9024c86b2d065adb0ad9881fca3b844aae
Andrey <zoxy1@yandex.ru>
2016-05-09 18:31:22 +0600

    +

--- labs/labs/main.cpp
+++ labs/labs/main.cpp
@@ -10,8 +10,18 @@
 
 #include <iostream> // содержит функции ввода вывода cin, cout
 #include<windows.h> //содержит функции SetConsoleCP(), SetConsoleOutputCP()
+#include <string>
 using namespace std;
-		
+
+struct uch_struct
+{
+string Familia;
+unsigned char kod;
+unsigned int ball;
+unsigned int mesto;
+};
+
+
 void main()
 {
 	SetConsoleCP(1251);// установка кодовой страницы win-cp 1251 в поток ввода
@@ -19,6 +29,20 @@ void main()
 	/* Для правильного отображения русских символов в консоли нужно выбрать 
 	в свойствах консоли шрифт Lucida Console */
  
+	uch_struct uchastnik[4];
+	
+	for(int i=1;i<=3;i++)
+	{
+	cout<<"введите через пробел фамилию "<<i<<"-го участника:";
+	cin>>uchastnik[i].Familia;
+	cout<<endl<<"введите код команды:";
+	cin>>uchastnik[i].kod;
+	cout<<endl<<"введите балл команды:";
+	cin>>uchastnik[i].ball;
+	cout<<endl<<"введите итоговое место команды:";
+	cin>>uchastnik[i].mesto;
+	cout<<endl;
+	}
 
 
 
