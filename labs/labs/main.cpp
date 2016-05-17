2053b57197d88ad887c44634941fe9440c6558ce
Andrey <zoxy1@yandex.ru>
2016-05-17 13:02:11 +0600

    убрал производный класс

--- labs/labs/main.cpp
+++ labs/labs/main.cpp
@@ -26,9 +26,6 @@ void set_ab(float a1,float b1) //метод записывает в недост
 protected:
 // при помещении методов в раздел protected класса, эти методы будут не доступны из вне класса,
 //доступ будет открыт только классам, производным от данного класса.
-float a; 
-float b; 
-	
 	
 void summ() //метод вычисляет сумму чисел a и b
 {
@@ -56,32 +53,8 @@ double delity() //метод вычисляет деление числа a на
 }
 private: // После объявления private следуют данные и функции(методы), не доступны внешним для класса функциям,
 		 // но они доступны для методов находящихся внутри класса
-
-};
-
-class Second_calculate : public calculate   // производный класс от класса calculate
-{
-public:
-
-int summ1;
-	Second_calculate() : calculate ()   // конструктор класса Second_calculate вызывает конструктор класса calculate
-{};
-void summ_second() //метод вычисляет сумму чисел a и b
-{
-	summ();
-}
-void minus_second() //метод вычисляет разность чисел a и b
-{
-	minus();
-}
-double umnogit_second() //метод вычисляет произведение чисел a и b
-{
-	return umnogit();
-}
-double delity_second() //метод вычисляет деление числа a на b
-{
-	return delity();
-} 
+float a; 
+float b; 
 };
 
 void main()
@@ -91,7 +64,6 @@ void main()
 	/* Для правильного отображения русских символов в консоли нужно выбрать 
 	в свойствах консоли шрифт Lucida Console */
 calculate *calculate_ptr=new calculate();  // создаем объект класса calculate
-Second_calculate * Second_calculate_ptr=new Second_calculate(); // создаем объект производного класса Second_calculate от класса calculate
 cout<<"Введите число а:";
 float a;
 cin>>a;
@@ -105,28 +77,18 @@ cin>>znak;
 switch(znak) //в зависимости от введенного символа выполняем арифметическое действие
 {
 	case '+':
-		Second_calculate_ptr->summ_second();
-		//Second_calculate_ptr->summ_second(); 
-	//т к не метод summ() находится в разделе protected класса calculate,
-	// то мы получае доступ к нему через производный класс Second_calculate через метод summ_second()
+	cout<<"a+b="<<calculate_ptr->summ()<<endl; // ошибка
 	break;
 	case '-':
-	//Second_calculate_ptr->minus_second(); 
-	//т к не метод summ() находится в разделе protected класса calculate,
-	// то мы получае доступ к нему через производный класс Second_calculate через метод summ_second()
+	cout<<"a-b="<<calculate_ptr->minus()<<endl; // ошибка
 	break;
 	case '*':
-	//cout<<"a*b="<<Second_calculate_ptr->umnogit_second()<<endl; 
-	//т к не метод summ() находится в разделе protected класса calculate,
-	// то мы получае доступ к нему через производный класс Second_calculate через метод summ_second()
+	cout<<"a*b="<<calculate_ptr->umnogit()<<endl; // ошибка
 	break;
 	case '/':
-	//cout<<"a/b="<<Second_calculate_ptr->delity_second()<<endl; 
-	//т к не метод summ() находится в разделе protected класса calculate,
-	// то мы получае доступ к нему через производный класс Second_calculate через метод summ_second()
-	
-	//без производного класса от класса calculate доступа к методам summ(), minus(),umnogit(), delity() нету, 
-	//т к они находятся в разделе protected 
+	cout<<"a/b="<<calculate_ptr->delity()<<endl; // ошибка
+	// выдаст ошибку, т к функции summ(),minus(),umnogit(),delity() находятся в разделе protected,
+	// и доступ будет открыт только классам, производным от класса calculate.
 	break;
 	default: cout<<"Введен не правильный символ арифметического действия"<<endl;
 }
