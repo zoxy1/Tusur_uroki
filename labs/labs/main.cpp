e9de23548223ce0839ce23ae2c6dcd3f350e3a16
Andrey <zoxy1@yandex.ru>
2016-05-22 02:14:04 +0600

    +

--- labs/labs/main.cpp
+++ labs/labs/main.cpp
@@ -1,105 +1,61 @@
 ﻿// lab11_2
-/* Написать программу, в которой описана иерархия классов: 
-функция от одной переменной (SIN(X), COS(X), TG(X)). 
-Базовый класс должен иметь методы получения значения функции 
-для данного значения переменной вводимой пользователем с экрана, 
-а также создания экземпляра класса, представляющего собой производную текущего экземпляра.
+/* Реализовать абстрактный базовый класс, определив в нем чистые виртуальные функции. 
+Эти функции определяются в производных классах. В базовых классах должны быть объявлены 
+чистые виртуальные функции ввода/вывода, которые реализуются в производных классах. 
+Вызывающая программа должна продемонстрировать все варианты вызова виртуальных функций 
+с помощью указателей на базовый класс. Написать функцию вывода, получающую параметры 
+базового класса по ссылке и демонстрирующую виртуальный вызов.
 */
 #include <iostream> // содержит функции ввода вывода cin, cout
-#include<windows.h> //содержит функции SetConsoleCP(), SetConsoleOutputCP()
-#include<math.h>
-#define PI  3.14159265358979323846
 using namespace std;
-
-class X // описываем базовый класс
-{
-public: // После объявления public следуют данные и функции(методы), доступные внешним для класса функциям.
-	X() // конструктор
-	{}
-	~X() // деструктор
-	{}
-	public:
-	virtual float function_x(float x1) 
-	{
-		return 0;
-	};  
-};
-class sin_X : public X
+class in_out // описываем базовый абстрактный класс
 {
 public:
-	float function_x(float x1)
-	{
-		return sin(x1 * PI / 180); 
-	}
+   virtual int in() const = 0; // чисто виртуальная функция
+   virtual void out(char *T) const = 0; // чисто виртуальная функция
 
-};
-class cos_X : public X
-{
-public:
-	float function_x(float x1)
+void set_text(char *txt)
 	{
-		return cos(x1 * PI / 180); 
+	text=txt;
 	}
-
-};
-class tan_X : public X
+char * get_text()
 {
-public:
-	float function_x(float x1)
-	{
-	return tan(x1 * PI / 180); 
-	}
-
-};
-class sin_X_rad : public sin_X
-{
-public:
-	float function_x(float x1)
-	{
-		return sin(x1); 
-	}
-
-};
-class cos_X_rad : public cos_X
-{
-public:
-	float function_x(float x1)
-	{
-		return cos(x1); 
-	}
-
-};
-class tan_X_rad : public tan_X
-{
-public:
-	float function_x(float x1)
-	{
-		return tan(x1); 
-	}
-
-};
-void main()
-{
-	SetConsoleCP(1251);// установка кодовой страницы win-cp 1251 в поток ввода
-    SetConsoleOutputCP(1251); // установка кодовой страницы win-cp 1251 в поток вывода
-	/* Для правильного отображения русских символов в консоли нужно выбрать 
-	в свойствах консоли шрифт Lucida Console */
-	
-	sin_X * sin_X_ptr=new sin_X();
-	cos_X * cos_X_ptr=new cos_X();
-	tan_X * tan_X_ptr=new tan_X();
-	sin_X_rad *sin_X_rad_ptr=new sin_X_rad();
-	cos_X_rad *cos_X_rad_ptr=new cos_X_rad();
-	tan_X_rad *tan_X_rad_ptr=new tan_X_rad();
-	cout<<"Введите значение х:";
-	float x_in;
-	cin>>x_in;
-	cout<<"sin("<<x_in<<"град.)="<<sin_X_ptr->function_x(x_in)<<endl;
-	cout<<"sin("<<x_in<<"рад.)="<<sin_X_rad_ptr->function_x(x_in)<<endl;
-	cout<<"cos("<<x_in<<"град.)="<<cos_X_ptr->function_x(x_in)<<endl;
-	cout<<"sin("<<x_in<<"рад.)="<<cos_X_rad_ptr->function_x(x_in)<<endl;
-	cout<<"tan("<<x_in<<"град.)="<<tan_X_ptr->function_x(x_in)<<endl;
-	cout<<"sin("<<x_in<<"рад.)="<<cos_X_rad_ptr->function_x(x_in)<<endl;
-	system("pause"); // команда задержки  экрана
+	return text;
 }
 
+private:
+char *text;
+};
+class vivod_text: public in_out // описываем производный класс kvadrat от класса Figura
+{
+public:
+   vivod_text () {} //конструктор принимает значение стороны квадрата, и записывает его в x1 
+  virtual void out(char *T) const
+  {
+  cout<<T<<endl;
+  }
+   virtual int in() const // определяем виртуальную функцию area() для расчета площади квадрата
+	{ 
+	int temp;
+		cout<<"Введите текст"<<endl;
+	cin>>temp;
+	return temp;
+  }
+};
+
+void main()
+{
+
+
+
+	in_out *vivod1; // инициализируем указатель figura1 типа Figura
+	vivod1=new vivod_text();
+	vivod1->in();
+	vivod1->out("asdasd");
+	//figura1=new kvadrat (5); // создаем объект класса kvadrat и присваиваем указателю figura1 его адрес 
+	//Figura *figura2; // инициализируем указатель figura1 типа Figura
+	//figura2=new krug (5); // создаем объект класса krug и присваиваем указателю figura2 его адрес 
+	//cout<<figura1->area()<<endl; //выводим возращаемое функцией area() значение площади квадрата
+	//cout<<figura2->area()<<endl; //выводим возращаемое функцией area() значение площади круга
+	system("pause"); // команда задержки  экрана
+}
